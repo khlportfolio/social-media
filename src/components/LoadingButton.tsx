c95d23b540dd51c328db210c2109491e69dad03a
rafi761 <khalif_rafi12@student.uns.ac.id>
2024-07-30 07:46:39 +0700

    Add Lucia auth setup & Add Navbar with session provider

---
+++ src/components/LoadingButton.tsx
@@ -0,0 +1,18 @@
+import { cn } from "@/lib/utils"
+import { Loader2 } from "lucide-react"
+import { Button, ButtonProps } from "./ui/button"
+
+interface LoadingButtonProps extends ButtonProps {
+    loading: boolean
+}
+
+const LoadingButton = ({ loading, disabled, className, ...props }: LoadingButtonProps) => {
+    return (
+        <Button disabled={loading || disabled} className={cn("flex items-center gap-2", className)} {...props}>
+            {loading && <Loader2 className="size-5 animate-spin" />}
+            {props.children}
+        </Button>
+    )
+}
+
+export default LoadingButton
