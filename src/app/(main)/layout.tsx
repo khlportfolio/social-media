7f0f50f26d887fc70d257c3f72b44ac52848b0e5
rafi761 <khalif_rafi12@student.uns.ac.id>
2024-07-30 09:30:09 +0700

    Add responsive sticky sidebar/bottom bar

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,5 +1,6 @@
 import { validateRequest } from "@/auth";
 import { redirect } from "next/navigation";
+import MenuBar from "./MenuBar";
 import Navbar from "./Navbar";
 import SessionProvider from "./SessionProvider";
 
@@ -12,9 +13,11 @@ export default async function Layout({ children }: { children: React.ReactNode }
         <SessionProvider value={session}>
             <div className="flex flex-col min-h-screen">
                 <Navbar />
-                <div className="max-w-7xl mx-auto p-5">
+                <div className="max-w-7xl mx-auto p-5 flex w-full grow gap-5">
+                    <MenuBar className="sticky top-[5.25rem] h-fit hidden sm:block flex-none space-y-3 rounded-2xl bg-card px-3 py-5 lg:px-5 shadow-sm xl:w-80" />
                     {children}
                 </div>
+                <MenuBar className="sticky bottom-0 flex w-full justify-center gap-5 border-t bg-card p-3 sm:hidden" />
             </div>
         </SessionProvider>
     )
