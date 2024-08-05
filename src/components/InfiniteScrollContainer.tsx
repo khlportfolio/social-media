7366d9d05be34d3a01e0af60171526c872cefa99
rafi761 <khalif_rafi12@student.uns.ac.id>
2024-08-05 18:52:44 +0700

    Adding infinite loading

---
+++ src/components/InfiniteScrollContainer.tsx
@@ -0,0 +1,27 @@
+import { useInView } from "react-intersection-observer"
+
+interface InfiniteScrollContainerProps extends React.PropsWithChildren {
+    onBottomReached: () => void;
+    className?: string;
+}
+
+import React from 'react'
+
+const InfiniteScrollContainer = ({ children, onBottomReached, className }: InfiniteScrollContainerProps) => {
+    const { ref } = useInView({
+        rootMargin: "200px",
+        onChange(inView) {
+            if (inView) {
+                onBottomReached();
+            }
+        }
+    })
+    return (
+        <div className={className}>
+            {children}
+            <div ref={ref} />
+        </div>
+    )
+}
+
+export default InfiniteScrollContainer
