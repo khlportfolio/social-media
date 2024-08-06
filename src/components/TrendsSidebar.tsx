9a5278769d98896a27c4b0aeddfc5e5408dd3d4f
rafi761 <khalif_rafi12@student.uns.ac.id>
2024-08-06 21:09:11 +0700

    Adding follow feature

--- src/components/TrendsSidebar.tsx
+++ src/components/TrendsSidebar.tsx
@@ -1,11 +1,12 @@
 import { validateRequest } from "@/auth";
 import prisma from "@/lib/prisma";
-import { userDataSelect } from "@/lib/types";
+import { getUserDataSelect } from "@/lib/types";
 import { formatNumber } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
 import { unstable_cache } from "next/cache";
 import Link from "next/link";
 import { Suspense } from "react";
+import FollowButton from "./FollowButton";
 import { Button } from "./ui/button";
 import UserAvatar from "./UserAvatar";
 
@@ -33,9 +34,14 @@ async function WhoToFollow() {
         where: {
             NOT: {
                 id: user.id
+            },
+            followers: {
+                none: {
+                    followerId: user.id
+                }
             }
         },
-        select: userDataSelect,
+        select: getUserDataSelect(user.id),
         take: 5
     })
 
@@ -54,7 +60,12 @@ async function WhoToFollow() {
                         </div>
                     </Link>
 
-                    <Button>Follow</Button>
+                    <FollowButton userId={user.id} initialState={{
+                        followers: user._count.followers,
+                        isFollowedByUser: user.followers.some(
+                            ({ followerId }) => followerId === user.id
+                        )
+                    }} />
                 </div>
             ))}
         </div>
