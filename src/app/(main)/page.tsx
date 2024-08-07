012af30f87f117329edffd387c1588e5c018ddb1
rafi761 <khalif_rafi12@student.uns.ac.id>
2024-08-07 14:43:00 +0700

    Adding following feed post and Adding user profile page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,8 +1,7 @@
 import PostEditor from "@/components/posts/editor/PostEditor";
-import Post from "@/components/posts/Post";
 import TrendsSidebar from "@/components/TrendsSidebar";
-import prisma from "@/lib/prisma";
-import { postDataInclude } from "@/lib/types";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import FollowingFeed from "./FollowingFeed";
 import ForYourFeed from "./ForYourFeed";
 
 export default function Home() {
@@ -10,7 +9,18 @@ export default function Home() {
     <main className="w-full min-w-0 flex gap-5">
       <div className="w-full min-w-0 space-y-5">
         <PostEditor />
-        <ForYourFeed />
+        <Tabs defaultValue="for-you">
+          <TabsList>
+            <TabsTrigger value="for-you">For you</TabsTrigger>
+            <TabsTrigger value="following">Following</TabsTrigger>
+          </TabsList>
+          <TabsContent value="for-you">
+            <ForYourFeed />
+          </TabsContent>
+          <TabsContent value="following">
+            <FollowingFeed />
+          </TabsContent>
+        </Tabs>
       </div>
       <TrendsSidebar />
     </main>
