81a1b549fad75f07c3409be6f689368c1b4db807
rafi761 <khalif_rafi12@student.uns.ac.id>
2024-08-14 21:59:31 +0700

    Adding cron job to delete orphaned uploads

---
+++ src/app/api/clear-uploads/route.ts
@@ -0,0 +1,56 @@
+import prisma from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+import { UTApi } from "uploadthing/server";
+
+export async function GET(req: NextRequest) {
+  try {
+    const authHeader = req.headers.get("Authorization");
+
+    if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+      return NextResponse.json(
+        { message: "Invalid authorization header" },
+        { status: 401 },
+      );
+    }
+
+    const unusedMedia = await prisma.media.findMany({
+      where: {
+        postId: null,
+        ...(process.env.NODE_ENV === "production"
+          ? {
+              createdAt: {
+                lte: new Date(Date.now() - 1000 * 60 * 60 * 24),
+              },
+            }
+          : {}),
+      },
+      select: {
+        id: true,
+        url: true,
+      },
+    });
+
+    new UTApi().deleteFiles(
+      unusedMedia.map(
+        (m) =>
+          m.url.split(`/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}/`)[1],
+      ),
+    );
+
+    await prisma.media.deleteMany({
+      where: {
+        id: {
+          in: unusedMedia.map((m) => m.id),
+        },
+      },
+    });
+
+    return new NextResponse();
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 },
+    );
+  }
+}
