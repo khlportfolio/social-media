12d762d3fc998d9b21911ffce44b2833aae8302b
rafi761 <khalif_rafi12@student.uns.ac.id>
2024-08-14 20:31:40 +0700

    Adding post media upload

--- src/components/posts/Post.tsx
+++ src/components/posts/Post.tsx
@@ -2,7 +2,9 @@
 
 import { useSession } from "@/app/(main)/SessionProvider"
 import { PostData } from "@/lib/types"
-import { formatRelativeDate } from "@/lib/utils"
+import { cn, formatRelativeDate } from "@/lib/utils"
+import { Media } from "@prisma/client"
+import Image from "next/image"
 import Link from "next/link"
 import Linkify from "../Linkify"
 import UserAvatar from "../UserAvatar"
@@ -47,8 +49,59 @@ const Post = ({ post }: PostProps) => {
                     {post.content}
                 </div>
             </Linkify>
+
+            {!!post.attachments.length && (
+                <MediaPreviews attachments={post.attachments} />
+            )}
         </article>
     )
 }
 
 export default Post
+
+
+interface MediaPreviewsProps {
+    attachments: Media[];
+}
+
+function MediaPreviews({ attachments }: MediaPreviewsProps) {
+    return (
+        <div className={cn("flex flex-col gap-3", attachments.length > 1 && "sm:grid sm:grid-cols-2")}>
+            {attachments.map(m => (
+                <MediaPreview key={m.id} media={m} />
+            ))}
+        </div>
+    )
+}
+
+interface MediaPreviewProps {
+    media: Media
+}
+
+function MediaPreview({ media }: MediaPreviewProps) {
+    if (media.type === "IMAGE") {
+        return (
+            <Image
+                src={media.url}
+                alt="Attachment"
+                width={500}
+                height={500}
+                className="mx-auto size-fit max-h-[30rem] rounded-2xl"
+            />
+        )
+    }
+
+    if (media.type === "VIDEO") {
+        return (
+            <div>
+                <video
+                    src={media.url}
+                    controls
+                    className="mx-auto size-fit max-h-[30rem] rounded-2xl"
+                />
+            </div>
+        )
+    }
+
+    return <p className="text-destructive">Unsupported media type</p>
+}
